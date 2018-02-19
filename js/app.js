3c9aed09cfed9d7ddc6477fc41de7b550265c09d
Cam O'Brien <camobrien343@gmail.com>
2018-02-19 15:57:11 -0600

    changed employee modal object and index.html

--- js/app.js
+++ js/app.js
@@ -4,33 +4,123 @@
 	const modal = document.querySelector('#modal');
 	const modalContent = document.querySelector('.modal-content');
 	const close = document.querySelector('.close');
+	const path = "https://randomuser.me/api/?results=12&nat=us";
 
-	const randomUserAPI = "https://randomuser.me/api/?results=12&nat=us";
-
-	const state = {
-	    employee: [],
-	    modalOpen: false,
-	    modalUserIndex: 0
+	let state = {
+	    user: [],
+	    filteredUsers: [],
+	    modalWindow: false,
+	    modalIndex: 0
 	};
 
-	function loadURL(randomUserAPI, callback) {
-		const xhr = new XMLHttpRequest();
-		
-		xhr.onreadystatechange = function() {
-			if ( this.readyState !== 4 ) {return;}
-			if ( this.readyState == 4 && this.status == 200 ) {
-				console.log(xhr);
-				var jsonResponse = JSON.parse(xhr.response).results;
-				callback(jsonResponse);
-			} else {
-				console.log('The request has failed.');
-			}
 
-			console.log('This always will run.');
-		};
-        xhr.open( 'GET', randomUserAPI );
-        xhr.send();
+
+	let getData = {
+		loadJSON: function(path, callback) {
+			const xhr = new XMLHttpRequest();
+			xhr.onreadystatechange = function() {
+				if ( this.readyState !== 4 ) {return;}
+				if ( this.readyState == 4 && this.status == 200 ) {
+					console.log(xhr);
+
+
+					const jsonResponse = JSON.parse(xhr.response).results;
+					callback(jsonResponse);
+				} else {
+					console.log('The request has failed.');
+				}
+				console.log('This always will run.');
+			};
+			xhr.open( 'GET', path );
+			xhr.send();
+		}
 	}
+
+	
+
+	let render = {
+		employeeCards: function() {
+		const container = document.querySelector('.container');
+		container.innerHTML = '';
+		state.user.forEach((user, index) => {
+		    let userCard = document.createElement("div");
+		    userCard.classList = 'user-card';
+		    userCard.innerHTML =
+
+        	    `<img src="${user.picture.medium}">
+        	       <div class="user-info">
+        	        <h2 class="user-name">${titleCase(user.name.first)} ${titleCase(user.name.last)}</h2>
+        	        <p class="email">${user.email}</p>
+        	        <p class="location">${titleCase(user.location.city)}</p>
+        	       </div>
+        	   </div>`;
+		    userCard.addEventListener('click', event => {
+		    	console.log('emloyee card clicked')
+		        state.modalWindow = true;
+		        state.modalIndex = index;
+		        this.employeeModal();
+		    });
+		    container.appendChild(userCard);
+		  });
+		},
+		employeeModal: function() {
+			if (state.modalWindow) {
+			    document.body.style.overflow = 'hidden';
+			    var user = state.user[state.modalIndex];
+			    // var d = user.dob.slice(0, 10).split('-');
+
+			    modalContent.innerHTML =
+		        	`<button id="close" class="close">X</button>
+	    	            <img src=${user.picture.large}>
+	    	            <h2 class="user-name">${titleCase(user.name.first)} ${titleCase(user.name.last)}</h2>
+	    	            <p class="email">${user.email}</p>
+	    	            <p class="location">${titleCase(user.location.city)}</p>
+
+	    	            
+	                    <p class="phone">${user.phone}</p>
+	    	            <p class="address">${titleCase(user.location.street)}, ${titleCase(user.location.city)}, ${user.location.postcode}, ${user.nat}</p>
+	    	            
+	                    <div class="modal-buttons">
+	                    <button id="previous" class="previous">Previous</button>
+	                    <button id="next" class="next">Next</button>
+	                    </div>
+	    	        </div>`
+			  
+			    modalContent.querySelector('.close').addEventListener('click', function(event) {
+			        console.log(event);
+	                event.preventDefault();
+			        state.modalWindow = false;
+			        render.employeeModal();
+			    },this);
+	            modalContent.querySelector('.previous').addEventListener('click', function(event) {
+	                if (state.modalIndex === 0) {
+	                    state.modalIndex = state.user.length - 1;
+	                } else {
+	                    state.modalIndex --;
+	                }
+	                render.employeeModal();
+	            },this);
+	            modalContent.querySelector('.next').addEventListener('click', function(event) {
+	                if (state.modalIndex === state.user.length - 1) {
+	                    state.modalIndex = 0;
+	                } else {
+	                    state.modalIndex ++;
+	                }
+	                render.employeeModal();
+	            },this);
+
+			    modal.style.display = "block";
+			} else {
+			    document.body.style.overflow = '';
+			    modal.style.display = "none";
+			}
+		}
+
+	}
+
+
+
+
 
 	function makeElement(type) {
 		return document.createElement(type);
@@ -46,87 +136,15 @@
     }
 
 
-	function render() {
-		if (state.modalOpen) {
-		    document.body.style.overflow = 'hidden';
-		    var user = state.employee[state.modalUserIndex];
-		    var d = user.dob.slice(0, 10).split('-');
-
-		    modalContent.innerHTML =
-	        	`<button id="close" class="close">X</button>
-    	            <img src=${user.picture.large}>
-    	            <h2 class="user-name">${titleCase(user.name.first)} ${titleCase(user.name.last)}</h2>
-    	            <p class="email">${user.email}</p>
-    	            <p class="location">${titleCase(user.location.city)}</p>
-
-    	            
-                    <p class="phone">${user.phone}</p>
-    	            <p class="address">${titleCase(user.location.street)}, ${titleCase(user.location.city)}, ${user.location.postcode}, ${user.nat}</p>
-    	            <p class="Birthday">Birthday: ${d[1]}/${d[2]}/${d[0]}</p>
-                    <div class="modal-buttons">
-                    <button id="previous" class="previous">Previous</button>
-                    <button id="next" class="next">Next</button>
-                    </div>
-    	        </div>`
-		  
-		    modalContent.querySelector('.close').addEventListener('click', function(event) {
-		        console.log(event);
-                event.preventDefault();
-		        state.modalOpen = false;
-		        render();
-		    });
-            modalContent.querySelector('.previous').addEventListener('click', function(event) {
-                if (state.modalUserIndex === 0) {
-                    state.modalUserIndex = state.employee.length - 1;
-                } else {
-                    state.modalUserIndex --;
-                }
-                render();
-            });
-            modalContent.querySelector('.next').addEventListener('click', function(event) {
-                if (state.modalUserIndex === state.employee.length - 1) {
-                    state.modalUserIndex = 0;
-                } else {
-                    state.modalUserIndex ++;
-                }
-                render();
-            });
-
-		    modal.style.display = "block";
-		} else {
-		    document.body.style.overflow = '';
-		    modal.style.display = "none";
-		}
 	
-		const container = document.querySelector('.container');
-		container.innerHTML = '';
-		state.employee.forEach((user, index) => {
-		    let userCard = makeElement("div");
-		    userCard.classList = 'user-card';
-		    userCard.innerHTML =
-        	    `<img src="${user.picture.medium}">
-        	       <div class="user-info">
-        	        <h2 class="user-name">${titleCase(user.name.first)} ${titleCase(user.name.last)}</h2>
-        	        <p class="email">${user.email}</p>
-        	        <p class="location">${titleCase(user.location.city)}</p>
-        	       </div>
-        	   </div>`;
-		    userCard.addEventListener('click', event => {
-		        state.modalOpen = true;
-		        state.modalUserIndex = index;
-		        render();
-		    });
-		    container.appendChild(userCard);
-		});
+	function init() {
+	    getData.loadJSON(path, user => {
+	    	console.log(user);
+	        state.user = user;
+	        render.employeeCards();
+	    });
 	}
 
-		function init() {
-		    loadURL(randomUserAPI, employee => {
-		    	console.log(employee);
-		        state.employee = employee;
-		        render();
-		    });
-		}
-
 	init();
+
 })();
